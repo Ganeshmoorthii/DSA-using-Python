3360e73557e1afba57ae6d417b0d782055d1c179
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2026-07-15 11:07:11 +0530

    Add new submission for Single Number (python3)

--- Single Number.py
+++ Single Number.py
@@ -4,3 +4,16 @@ class Solution:
         for i in nums:
             xor = xor^i
         return xor
+
+---
+
+## Submission — 15 Jul 2026, 11:07 am (PYTHON3)
+
+```py
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor=0
+        for i in nums:
+            xor = xor^i
+        return xor
+```
